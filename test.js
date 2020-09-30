a28d36221d97b6b549251411245565ee23d60b2a
notchla <liso.lorenzo@gmail.com>
2020-09-30 19:07:57 +0200

    removed test

--- test.js
+++
@@ -1,9 +0,0 @@
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user:TecWeb@cluster0.rbdoz.mongodb.net/sample_geospatial?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected")
-  client.close();
-});
