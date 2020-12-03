82a6193a1b0a4211c204508b46dbd19388fd8608
notchla <liso.lorenzo@gmail.com>
2020-12-03 17:36:02 +0100

    added open question handler

--- public/js/open question.js
+++ public/js/open question.js
@@ -1,3 +1,30 @@
 function activity_checker() {
-  function handle_response(response) {}
+  function handle_response(response) {
+    console.log(response);
+    const index = story_data.nodes[current_node].content.answer.findIndex(
+      (el) => el === response
+    );
+    if (index != -1) {
+      //todo handle error for wrong response, needs support in creator
+      const adjIndex = getAdjIndex();
+      const adj = story_data.adj[adjIndex];
+      setAdjIndex(adj.v[index]);
+      const nodeIndex = getNodeIndex(adj.v[index]);
+      current_node = nodeIndex;
+      updateContent(story_data.nodes[nodeIndex]);
+    } else {
+      alert("error loading next activity");
+    }
+  }
+  // $("#response button").click(function (event) {
+  //   console.log($(this).text());
+  //   handle_response($(this).text());
+  // });
+
+  document.getElementById("response").addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const form = evt.target;
+    const response = form.elements["response-text"].value;
+    handle_response(response);
+  });
 }
