fa98ad82595e96b9c0ad36afb2120aadf5e4929b
gerkone <gianlucagalletti@ymail.com>
2020-12-26 19:48:39 +0100

    better evaluator view

--- public/js/evaluator.js
+++ public/js/evaluator.js
@@ -3,44 +3,49 @@ function adduser(name, activityID, time, sessionID, username) {
   $("#users")
     .append(`<a href="#" class="list-group-item list-group-item-action border-0" id ="${sessionID}">
     <div class="d-flex align-items-start">
-      <img
-        src="https://bootdey.com/img/Content/avatar/avatar5.png"
-        class="rounded-circle mr-1"
-        alt="${username}"
-        width="40"
-        height="40"
-      />
       <div class="flex-grow-1 ml-3 userInfo">
-        ${username}
-        activity : ${activityID}
-        time : ${time}
-        id : ${sessionID}
-        story : ${name}
-        <div class="small">
-          <span class="fas fa-circle chat-online"></span>
-          Online
+        <div class="d-flex w-100 justify-content-between">
+          <h4> ${username} </h4>
+          <small class="timer mt-1"> ${time} </small>
+        </div>
+        <div class="ml-2 row">
+          <p class="ml-0 mr-auto" style="font-weight: lighter; color:#666666;">
+          In story <span style="font-weight: bold;""> ${name} </span> on activity ${activityID}
+          </p>
+          <div class="ml-auto mr-3 badge-container"></div>
         </div>
       </div>
     </div>
   </a>`);
 }
 
-function updateUser(user, name, activityID, time, sessionID, username) {
+function updateUser(user, name, activityID, time, username) {
   // console.log(user.)
   var info = user.getElementsByClassName("userInfo");
   $(info).after(`<div class="flex-grow-1 ml-3 userInfo">
-  ${username}
-  activity : ${activityID}
-  time : ${time}
-  id : ${sessionID}
-  story : ${name}
-  <div class="small">
-    <span class="fas fa-circle chat-online"></span>
-    Online
-  </div>
+    <div class="d-flex w-100 justify-content-between">
+      <h4> ${username} </h4>
+      <small class="timer mt-1"> ${time} </small>
+    </div>
+    <div class="ml-2 row">
+      <p class="ml-0 mr-auto" style="font-weight: lighter; color:#666666;">
+      In story <span style="font-weight: bold;""> ${name} </span> on activity ${activityID}
+      </p>
+      <div class="ml-auto mr-3 badge-container"></div>
+    </div>
 </div>`);
 
   $(info)[0].remove();
+}
+
+function msToTime(s) {
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+  return hrs + " h " + mins + " mins " + secs + " secs";
 }
 
 const userdata = {};
@@ -56,6 +61,8 @@ socket.emit("registerEvaluator", {});
 socket.emit("populateEvaluator", {});
 //todo populate evaluator, data is an array
 socket.on("populate", (data) => {
+  var d = new Date();
+  var now = d.getTime();
   data.forEach((user) => {
     console.log("user", user);
     var a = document.getElementById(user.sessionID);
@@ -67,15 +74,14 @@ socket.on("populate", (data) => {
         a,
         user.name,
         user.activityID,
-        user.time,
-        user.sessionID,
+        msToTime(now - user.time),
         user.username ? user.username : "anonymous"
       );
     } else {
       adduser(
         user.name,
         user.activityID,
-        user.time,
+        msToTime(now - user.time),
         user.sessionID,
         user.username ? user.username : "anonymous"
       );
@@ -91,4 +97,30 @@ socket.on("delete", (data) => {
   const id = "#" + data;
   $(id).remove();
   console.log("after delete ", userdata);
+});
+
+$(document).ready(function () {
+  // execute timers every 5 seconds
+  var d;
+  var now = 0;
+  var t = setInterval(() => {
+    d = new Date();
+    now = d.getTime();
+    for (const [_, user] of Object.entries(userdata)) {
+      // update time indicators
+      var elapsed = now - user.time;
+      var timer = $("#" + user.sessionID + " .timer").text(msToTime(elapsed));
+      if((elapsed > 5 * 60 * 1000) && (!userdata.notified)) {
+        // stuck for long time, add badge
+        userdata.notified = true;
+        $("#" + user.sessionID + " .badge-container").prepend('<div class="badge bg-warning">' +
+          '! ! !' +
+        '</div>');
+      } else if ((elapsed < 5 * 60 * 1000)) {
+        // remove badge, proceeded to next activity
+        userdata.notified = false;
+        $("#" + user.sessionID + " .badge").remove();
+      }
+    }
+  }, 5);
 });
